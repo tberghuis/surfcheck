7ab2608134cd67bb29b200f4029e97331616a2c8
Thomas Berghuis <thomas.berghuis@gmail.com>
2020-05-04 17:33:13 +1000

    .

--- SurfcheckNavigation/app/src/main/java/site/thomasberghuis/surfcheck/BeachFragment.kt
+++ SurfcheckNavigation/app/src/main/java/site/thomasberghuis/surfcheck/BeachFragment.kt
@@ -1,16 +1,23 @@
 package site.thomasberghuis.surfcheck
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.hls.HlsMediaSource
+import com.google.android.exoplayer2.ui.PlayerView
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory
+import com.google.android.exoplayer2.util.Util
 
 
 class BeachFragment : Fragment() {
 
+    private var playerView: PlayerView? = null
+    private var exoplayer: SimpleExoPlayer? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,9 +27,44 @@ class BeachFragment : Fragment() {
 
         val safeArgs: BeachFragmentArgs by navArgs()
         val beachName = safeArgs.beach
-        val root = inflater.inflate(R.layout.fragment_beach, container, false)
+        val url = getBeachUrl(beachName)
 
+        val root = inflater.inflate(R.layout.fragment_beach, container, false)
+        setupPlayer(root, url)
         return root
+    }
+
+    private fun setupPlayer(v: View, url: String) {
+        playerView = v.findViewById(R.id.player_view)
+        exoplayer = SimpleExoPlayer.Builder(requireContext()).build();
+
+        playerView?.player = exoplayer
+
+//        val userAgent = Util.getUserAgent(baseContext, "Exo")
+        val userAgent = "whatever"
+
+        val mediaUri = Uri.parse(url)
+
+        val mediaSource = HlsMediaSource.Factory(DefaultHttpDataSourceFactory(userAgent))
+            .createMediaSource(mediaUri)
+
+        exoplayer?.prepare(mediaSource)
+
+        exoplayer?.playWhenReady = true
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        releasePlayer()
+    }
+
+    private fun releasePlayer() {
+        if (exoplayer != null) {
+            exoplayer?.stop()
+            exoplayer?.release()
+            exoplayer = null
+        }
     }
 
 
