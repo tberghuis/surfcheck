36bfb546c0c6cf6b29eb507fac12d91742397f55
Thomas Berghuis <thomas.berghuis@gmail.com>
2020-05-04 12:33:56 +1000

    .

--- surfcheck/app/src/main/java/site/thomasberghuis/surfcheck/MainActivity.kt
+++ surfcheck/app/src/main/java/site/thomasberghuis/surfcheck/MainActivity.kt
@@ -1,20 +1,11 @@
 package site.thomasberghuis.surfcheck
 
-import android.content.ComponentName
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v4.media.session.MediaSessionCompat
-import android.support.v4.media.session.PlaybackStateCompat
-import com.google.android.exoplayer2.ExoPlayerFactory
 import com.google.android.exoplayer2.SimpleExoPlayer
-import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory
-import com.google.android.exoplayer2.source.ExtractorMediaSource
 import com.google.android.exoplayer2.source.hls.HlsMediaSource
-import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
 import com.google.android.exoplayer2.ui.PlayerView
-import com.google.android.exoplayer2.ui.SimpleExoPlayerView
-import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory
 import com.google.android.exoplayer2.util.Util
 
@@ -22,8 +13,6 @@ class MainActivity : AppCompatActivity() {
 
     private var playerView: PlayerView? = null
     private var exoplayer: SimpleExoPlayer? = null
-    private var playbackStateBuilder: PlaybackStateCompat.Builder? = null
-    private var mediaSession: MediaSessionCompat? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
